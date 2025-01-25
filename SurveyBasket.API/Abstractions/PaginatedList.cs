a38ea1ed878dce6ef3c0f3c4941120b176a84809
Saleem Kassab <saleemkassab5@gmail.com>
2025-01-25 10:34:14 +0300

    Add Pagination Logic

---
+++ SurveyBasket.API/Abstractions/PaginatedList.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SurveyBasket.API.Abstractions
+{
+	public class PaginatedList<T>(int pageNumber, int count, int pageSize, List<T> items)
+	{
+		public int PageNumber { get; set; } = pageNumber;
+		public int TotalPages { get; set; } = (int)Math.Ceiling(count / (double)pageSize);
+		public List<T> Items { get; private set; } = items;
+
+
+		public bool HasPreviousPage => PageNumber > 1;
+		public bool HasNextPage => PageNumber < TotalPages;
+
+		public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> query, int pageNumber, int pageSize)
+		{
+			var count = await query.CountAsync();
+			var items = await query.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+
+			return new PaginatedList<T>(pageNumber, count, pageSize, items);
+		}
+	}
+}
